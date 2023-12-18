81ffb2eaf416ebfa55a50021b863850a47ba82c1
Hilol <khilol2017@gmail.com>
2023-12-18 05:44:02 +0300

    программы

---
+++ код/individualnoe.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    # Определим универсальное множество
+    u = set("abcdefghijklmnopqrstuvwxyz")
+    A = {"a", "b", "h", "k", "o", "r"}
+    B = {"b", "g", "h", "l", "s"}
+    C = {"k", "l", "z"}
+    D = {"g", "i", "p", "q", "u", "v"}
+    x = (A.intersection(B)).union(D)
+    print(f"x = {x}")
+    # дополнения множеств
+    an = u.difference(A)
+    bn = u.difference(B)
+    y = (an.intersection(bn)).difference(C.union(D))
+    print(f"y = {y}")
